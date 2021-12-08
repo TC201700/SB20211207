dafc017e589a14ce59f604f2f36418e284d6733b
example <example@nuc.fritz.box>
2021-12-08 17:25:42 +0000

    use addresses, not address, as name/path for address api

--- src/main/java/com/example/demo/address/AddressRepository.java
+++ src/main/java/com/example/demo/address/AddressRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.address;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "address", path = "address")
+@RepositoryRestResource(collectionResourceRel = "addresses", path = "addresses")
 public interface AddressRepository extends PagingAndSortingRepository<Address, Long> {
 
 }
